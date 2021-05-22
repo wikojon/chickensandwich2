d0700e22a3c9515a7a4fbd4c89cf9e85fcd58caa
wikojon <76674004+wikojon@users.noreply.github.com>
2021-05-21 20:36:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload()
+{
+clown_image = loadImage('https://i.postimg.cc/LXzjB15m/test.png')
+}
+
+noseX=0;
+noseY=0;
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is loaded')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results)
+        noseX = + results[0].pose.nose.x; 
+        noseY = + results[0].pose.nose.y ;
+    }
+}
+
+function draw()
+{
+image(video,0,0,300,300);
+image(clown_image,noseX -10,noseY -10,25,30);
+
+}
+function take_snapshot()
+{
+    save('chickenSandwich.png');
+}
